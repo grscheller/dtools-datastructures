f8aa339ca64639c68324b4deb22b2e12a1e2783e
grscheller <geoffrey@scheller.com>
2023-09-02 13:26:29 -0600

    Made pytest package a required dependency: v0.2.1.0
    - to ensure that a correct matching version of pytest is used
      - to not invoke an incompatible versions pf pytest
        - picked up on the shell path
        - using the wrong version of python or Python environment
    - bump version to 0.2.1.0 and will tag v0.2.1.0 on GitHub

--- grscheller/datastructures/tests/__init__.py
+++ grscheller/datastructures/tests/__init__.py
@@ -11,21 +11,17 @@ The testsuite can be either run against
 
 2. The pip installed package of a particular version from GitHub.
 
-   $ pip install pytest
-   $ pip install git+https://github.com/grscheller/datastructures@v0.2.0.2
+   $ pip install git+https://github.com/grscheller/datastructures@v0.2.1.0
    $ pytest --pyargs grscheller.datastructures
 
-3. The pip installed package from PyPI (NOT YET PUSHED TO PYPI!!!)
+3. The pip installed package from PyPI (NOT YET PUSHED TO PyPI!!!)
 
-   $ pip install pytest
    $ pip install grscheller.datastructures
    $ pytest --pyargs grscheller.datastructures
 
-By default, virtual environments created by venv don't give access to the site
-packages of the host Python used (at least this is the behavior for the
-externally managed System Python on Arch Linux). Therefore, pytest should be
-installed into whatever Python environment you are using. Otherwise, the wrong
-pytest executable running the wrong version of Python could be found on your
-shell $PATH.
+The pytest package was made a dependency of the grscheller.datastructures
+package. This was to ensure the correct matching version of pytest was used to
+run the tests. Otherwise, the wrong pytest executable running the wrong version
+of Python might be found on your shell $PATH.
    
 """ 
