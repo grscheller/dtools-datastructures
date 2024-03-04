fb41afd6c674abf7a34070a2c256859f4376967a
grscheller <geoffrey@scheller.com>
2024-03-04 07:18:33 -0700

    Forgot to update version number on last commit.

--- src/grscheller/datastructures/__init__.py
+++ src/grscheller/datastructures/__init__.py
@@ -14,7 +14,7 @@
 
 """Classes to implement algorithms, functional and imperative styles supported."""
 
-__version__ = "0.13.3.2"
+__version__ = "0.13.3.3"
 __author__ = "Geoffrey R. Scheller"
 __copyright__ = "Copyright (c) 2023-2024 Geoffrey R. Scheller"
 __license__ = "Apache License 2.0"
